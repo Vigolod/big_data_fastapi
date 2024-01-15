33956ffa217d82d473e4dc694d2593dfe2cd021e
Максим Демидов <myudemidov@edu.hse.ru>
2024-01-15 21:17:16 +0300

    added quoting vehilce names

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from functools import partial
+from urllib.parse import quote
+
 from fastapi import Request, Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
@@ -11,7 +14,8 @@ templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request, db=Depends(db_callback)):
-    vehicles_list = db.vehicles
+    vehicles_quoted = map(partial(quote, safe=""), db.vehicles)
     return templates.TemplateResponse(
-        'index.html', {"request": request, "vehicles": vehicles_list}
+        'index.html',
+        {"request": request, "vehicles": zip(db.vehicles, vehicles_quoted)}
     )
