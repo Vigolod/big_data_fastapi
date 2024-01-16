dde2c9848bdf2c5a2aa5b8b275495bf103b8609a
Vigolod <109913264+Vigolod@users.noreply.github.com>
2024-01-16 14:43:05 +0200

    fix output

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,17 @@
 def get_kds(columns, values):
-    kds = [val for col, val in zip(columns, values) if col in ["KDL", "KDH", "KDE", "KD"]]
+    col_val_dict = dict(zip(columns, values))
+    kd_dict = {}
+    for k, v in col_val_dict.items():
+        if k.startswith("KD"):
+            suffix = "" if k == "KD" else k[-1]
+            deaths = col_val_dict["TD" + suffix]
+            kills = col_val_dict['TK' + suffix]
+            if deaths == 0:
+                kd_dict[k] = f">={kills}"
+            elif kills == 0:
+                kd_dict[k] = f"<={(1 / deaths):.2f}"
+            else:
+                kd_dict[k] = f"{v:.2f}"
+    kds = [kd_dict[col] for col in ["KDL", "KDH", "KDE", "KD"]]
     return kds
 
